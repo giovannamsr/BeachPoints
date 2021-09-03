95aa9ea9a6ec5d5f624206d413ce158cd135c096
Giovanna Rodrigues <giovannamendessr@gmail.com>
2021-09-03 17:02:10 -0300

    Initial Commit

---
+++ BeachPoints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeachPoints
+//
+//  Created by Marcelo Rodrigues de Sousa on 03/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
